5cee8231c5d64fe85f45683a71c57e0d25635a49
一碟茴香豆 <dengzhaoke@outlook.com>
2023-03-29 17:55:09 +0800

    first commit

---
+++ src/pages/docs.tsx
@@ -0,0 +1,9 @@
+const DocsPage = () => {
+  return (
+    <div>
+      <p>Salve2 docs.</p>
+    </div>
+  );
+};
+
+export default DocsPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import yayJpg from '../assets/yay.jpg';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h2>Slave2</h2>
+    </div>
+  );
+}
