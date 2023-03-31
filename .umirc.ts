225cf5eec280095f171ba92fc0a35143ae81add1
一碟茴香豆 <dengzhaoke@outlook.com>
2023-03-31 16:51:08 +0800

    add mf

--- .umirc.ts
+++ .umirc.ts
@@ -1,14 +1,48 @@
 import { defineConfig } from "umi";
 
+const shared = {
+  react: {
+    singleton: true,
+    // eager: true,
+  },
+  "react-dom": {
+    singleton: true,
+    // eager: true,
+  },
+  
+};
+
 export default defineConfig({
-  title: '7002',
+  title: "7002",
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
   ],
-  npmClient: 'pnpm',
-  plugins: ["@umijs/plugins/dist/qiankun"],
+  npmClient: "pnpm",
+  plugins: [
+    "@umijs/plugins/dist/qiankun",
+    "@umijs/plugins/dist/model",
+    "@umijs/plugins/dist/mf",
+  ],
   qiankun: {
     slave: {},
+  },
+  model: {},
+  // mfsu: {
+  //   mfName: 'slave2'
+  // },
+  mf: {
+    name: 'hostUser',
+    remotes: [
+      {
+        name: 'remoteCounter',
+        entries: {
+          DEV: 'http://10.20.14.164:8000/remote.js',
+          PROD: 'https://production.com/remote.js',
+        },
+        keyResolver: `(()=> 'DEV')()`,
+      },
+    ],
+    shared,
   },
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,41 @@
-import yayJpg from '../assets/yay.jpg';
+import { useModel } from "umi";
+import { Button , DatePicker} from "antd";
+import React from 'react'
+// import wangeditor from 'component_app/wangeditor'
+
+const RemoteCounter = React.lazy(() => {
+  // @ts-ignore
+  return import('remoteCounter/Button');
+});
+
+
+const { RangePicker } = DatePicker;
 
 export default function HomePage() {
+  const materProps = useModel("@@qiankunStateFromMaster");
+
+  console.log('====================================');
+  console.log("React: ", RemoteCounter);
+  console.log('====================================');
+
   return (
     <div>
       <h2>Slave2</h2>
+      <h3>接受父应用参数：{materProps?.masterState?.slogan ?? "-"}</h3>
+      <Button
+        type="primary"
+        onClick={() => {
+          if (materProps?.setMasterState) {
+            materProps.setMasterState({
+              ...(materProps?.masterState || {}),
+              slogan: "xxx",
+            });
+          }
+        }}
+      >
+        xxx
+      </Button>
+      <RangePicker />
     </div>
   );
 }
